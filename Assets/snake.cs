356fcee6e14dec09bc8c94a1f79caae1faff5d0d
sxqing <3048604497@qq.com>
2023-10-28 20:14:23 +0800

    第一次提交

---
+++ Assets/snake.cs
@@ -0,0 +1,93 @@
+//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments = new List<Transform>();
+    public Transform segmentPrefab;
+    public int initialSize = 3;
+
+    private void Start()
+    {
+        //_segments = new List<Transform>();
+        //_segments.Add(this.transform);
+        ResetState();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (_direction != Vector2.down)
+                _direction = Vector2.up;
+        }
+        else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (_direction != Vector2.up)
+                _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (_direction != Vector2.right)
+                _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (_direction != Vector2.left)
+                _direction = Vector2.right;
+        }
+    }
+    private void FixedUpdate()
+    {
+        for(int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i-1].position;
+        }
+
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for (int i=1;i< _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for(int i=1;i<this.initialSize;i++)
+        {
+            _segments.Add(Instantiate(this.segmentPrefab)); 
+        }
+
+        this.transform.position= Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
+        else if(other.tag == "Obstacle")
+        {
+            ResetState();
+        }
+    }
+}
